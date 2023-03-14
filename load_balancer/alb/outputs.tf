8ec7c200af218ccbfe1ea2b12c565c39f6840e3a
Madhava Reddy SV <madhav@tarkalabs.com>
2023-03-14 16:16:02 -0300

    alb module output renamed

--- load_balancer/alb/outputs.tf
+++ load_balancer/alb/outputs.tf
@@ -1,24 +1,24 @@
-output "lb_id" {
+output "id" {
   description = "The ID and ARN of the load balancer we created"
   value       = module.alb.lb_id
 }
 
-output "lb_arn" {
+output "arn" {
   description = "The ID and ARN of the load balancer we created"
   value       = module.alb.lb_arn
 }
 
-output "lb_dns_name" {
+output "dns_name" {
   description = "The DNS name of the load balancer"
   value       = module.alb.lb_dns_name
 }
 
-output "lb_arn_suffix" {
+output "arn_suffix" {
   description = "ARN suffix of our load balancer - can be used with CloudWatch"
   value       = module.alb.lb_arn_suffix
 }
 
-output "lb_zone_id" {
+output "zone_id" {
   description = "The zone_id of the load balancer to assist with creating DNS records"
   value       = module.alb.lb_zone_id
 }
