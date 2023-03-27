f5674cb968301a3d8eb7092a88fa3233795cf924
Madhava Reddy SV <madhav@tarkalabs.com>
2023-03-27 18:35:39 -0300

    ebs csi driver provided by default and role will get created automatically

--- eks/cluster/main.tf
+++ eks/cluster/main.tf
@@ -42,6 +42,10 @@ module "eks" {
         }
       })
     }
+    aws-ebs-csi-driver           = {
+      most_recent                = true
+      service_account_role_arn   = module.ebs_csi_irsa.iam_role_arn
+    }
   })
 
   cluster_service_ipv4_cidr               = var.cluster_service_ipv4_cidr
@@ -94,6 +98,22 @@ module "vpc_cni_irsa" {
     main                 = {
       provider_arn               = module.eks.oidc_provider_arn
       namespace_service_accounts = ["kube-system:aws-node"]
+    }
+  }
+}
+
+module "ebs_csi_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.13"
+
+  role_name              = "${var.cluster_name}-ebs-csi-irsa-role"
+  attach_ebs_csi_policy  = true
+  tags                   = var.tags
+
+  oidc_providers         = {
+    main                 = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
     }
   }
 }
